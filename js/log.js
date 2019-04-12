5630b55230dde20f240984ac6858030df6bd22a7
Eric Poitras <EricPoitras@users.noreply.github.com>
2019-04-11 23:08:58 -0600

    Add files via upload

---
+++ js/log.js
@@ -0,0 +1,190 @@
+// Data logged for research purposes - stored in server, not retrieved...
+
+// Variables
+var logdata;
+
+var docurl = '';
+var d = '';
+var n = '';
+
+var docheight = '';
+var docwidth = '';
+var deviceheight = '';
+var devicewidth = '';
+var bodyheight = '';
+var bodywidth = '';
+var pagexoffset = '';
+var pageyoffset = '';
+
+var counter = 0;
+var timer = 0;
+
+// Functions
+
+//Session identifier
+function setsessionid(){
+    if (sessionStorage.getItem("sessionid") === null) {
+        var sessionid = Math.floor(Math.random()*1000000000001);
+        sessionStorage.sessionid = sessionid;
+        sessionStorage.batch_log = "";
+    } 
+}
+
+//Setting document url
+function setvariables(){
+    //Get document url
+    docurl = document.URL;
+    //Get timestamp
+    d = new Date();
+    n = d.toUTCString();
+}
+
+//Setting screen variables
+function setscreenvariables(){
+    //Viewport size
+    docheight = document.documentElement.clientHeight;
+    docwidth = document.documentElement.clientWidth;
+    //Device size
+    deviceheight = window.screen.height;
+    devicewidth = window.screen.width;
+    //Document size
+    bodyheight = document.body.clientHeight;
+    bodywidth = document.body.clientWidth;
+    //Pixel number scrolled from upper left corner of the document, horizontally and vertically
+    pagexoffset = window.pageXOffset;
+    pageyoffset = window.pageYOffset;
+}
+
+//Counter of logged events
+function incrementcounter(){
+    //Set counter
+    counter += 1;
+}
+
+function settimer(){
+    timer += 0.1;
+}
+
+setInterval("settimer()", 100);
+
+function refreshloggervalues(){
+    setsessionid();
+    setvariables();
+    setscreenvariables();
+    incrementcounter();
+}
+
+// Data Logging Function
+function datalogger(categorylabel, eventlabel, qualifierlabel){
+    
+    var newitem = {
+        session: sessionStorage.sessionid,
+        username: localStorage.UTTECusername,
+        classname: localStorage.classname,
+        timestamp: n,
+        timer: timer,
+        counter: counter,
+        page: docurl,
+        docheight: docheight,
+        docwidth: docwidth,
+        deviceheight: deviceheight,
+        devicewidth: devicewidth,
+        bodyheight: bodyheight,
+        bodywidth: bodywidth,
+        pagexoffset: pagexoffset,
+        pageyoffset: pageyoffset,
+        category: categorylabel,
+        event: eventlabel, 
+        qualifier: qualifierlabel
+    };
+    
+    localStorage.sessionlog = JSON.stringify(newitem);
+    
+    // Log research data to MySQL database - lab evaluation EP 11 10 2018
+    //$.post("admin/researchlog.php",{logtimestamp: n, logtimer: timer, logcounter: counter, logwebpage: docurl, loglabel: JSON.stringify(newitem)});
+    sessionStorage.batch_log = sessionStorage.batch_log + "\n" + "logtimestamp" + "," + n + "," + "logtimer" + "," + timer + "," + "logcounter" + "," + counter + "," + "logwebpage" + "," + docurl + "," + "loglabel" + "," + JSON.stringify(newitem);  
+}
+
+// Event Handlers
+
+$(window).on("load",function(){
+    refreshloggervalues();
+    datalogger("NavigationEvent","DocumentLoad","N/A");
+});
+
+$(window).on("unload",function(){
+    refreshloggervalues();
+    datalogger("NavigationEvent","DocumentUnload","N/A");
+});
+
+$(document).on("click","button, a, textarea",function(){
+    var element = $(this).html().replace(/\r?\n|\r|\s|["']/g,"");
+    var elementdescription = element.substring(0,500);
+    var elementType = $(this).prop('nodeName');
+    refreshloggervalues();
+    datalogger("ClickEvent",elementdescription,elementType);
+});
+
+$(document).on("dblclick","button, a, textarea",function(){
+    var element = $(this).html().replace(/\r?\n|\r|\s|["']/g,"");
+    var elementdescription = element.substring(0,500);
+    var elementType = $(this).prop('nodeName');
+    refreshloggervalues();
+    datalogger("DBLClickEvent",elementdescription,elementType);
+});
+
+$(document).on("click","input",function(){
+    var element = $(this).html().replace(/\r?\n|\r|\s|["']/g,"");
+    var elementdescription = element.substring(0,500);
+    var elementType = $(this).attr('id');
+    refreshloggervalues();
+    datalogger("ClickEvent",elementdescription,elementType);
+});
+
+$(document).on("dblclick","input",function(){
+    var element = $(this).html().replace(/\r?\n|\r|\s|["']/g,"");
+    var elementdescription = element.substring(0,500);
+    var elementType = $(this).attr('id');
+    refreshloggervalues();
+    datalogger("DBLClickEvent",elementdescription,elementType);
+});
+
+$(document).on("touchstart","button, a, input, textarea",function(){
+    var element = $(this).html().replace(/\r?\n|\r|\s|["']/g,"");
+    var elementdescription = element.substring(0,500);
+    var elementType = $(this).prop('nodeName');
+    refreshloggervalues();
+    datalogger("TouchStart",elementdescription,elementType);
+});
+
+$(document).on("touchend","button, a, input, textarea",function(){
+    var element = $(this).html().replace(/\r?\n|\r|\s|["']/g,"");
+    var elementdescription = element.substring(0,500);
+    var elementType = $(this).prop('nodeName');
+    refreshloggervalues();
+    datalogger("TouchEnd",elementdescription,elementType);
+});
+
+$(document).on("touchstart","input",function(){
+    var element = $(this).html().replace(/\r?\n|\r|\s|["']/g,"");
+    var elementdescription = element.substring(0,500);
+    var elementType = $(this).attr('id');
+    refreshloggervalues();
+    datalogger("TouchStart",elementdescription,elementType);
+});
+
+$(document).on("touchend","input",function(){
+    var element = $(this).html().replace(/\r?\n|\r|\s|["']/g,"");
+    var elementdescription = element.substring(0,500);
+    var elementType = $(this).attr('id');
+    refreshloggervalues();
+    datalogger("TouchEnd",elementdescription,elementType);
+});
+
+$(document).on("change paste keyup","textarea, input",function(){
+    var elementvalue = $(this).val();
+    //var elementdescription = elementvalue.toString().substring(0,500);
+    var elementcontent = $(this).prop('nodeName');
+    refreshloggervalues();
+    datalogger("InputEvent",elementvalue,elementcontent);
+});
---
+++ js/main.js
@@ -0,0 +1,238 @@
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawAxisTickColors);
+
+var tigercount = 0;
+var hippocount = 0;
+var monkeycount = 0;
+var groups = 3;
+var username;
+var classname;
+var listofbehaviors;
+var listofbehaviorsdisplay;
+
+function setdatafromlocalstorage(){
+    if (localStorage.getItem("UTTECusername") !== null) {
+        // Code for localStorage/sessionStorage.
+        console.log("Connection to local storage established...");
+        tigercount = parseInt(localStorage.tigercount);
+        hippocount =  parseInt(localStorage.hippocount);
+        monkeycount = parseInt(localStorage.monkeycount);
+        groups = parseInt(localStorage.groups);
+        username = localStorage.UTTECusername;
+        classname = localStorage.classname;
+        listofbehaviors = localStorage.listofbehaviors;
+        listofbehaviorsdisplay = localStorage.listofbehaviorsdisplay;
+        
+        $("#input_username").val(username);
+        $("#input_class").val(classname);
+         //$("title").text(username);
+         //$(".navbar-brand").html("<img src='assets/logo.svg' width='75' height='75' alt=''>Behavioral Management: "+classname);
+        try{
+         $("#listbehaviors").html(JSON.parse(listofbehaviors));
+         $("#listbehaviorsdisplay").html(JSON.parse(listofbehaviorsdisplay));
+        }catch(error){
+            console.log(error);
+        }
+           //document.getElementById("listbehaviordisplay").getElementsByClassName("fa-trash")[0].style.display = "none";
+         //$("#listbehaviordisplay .fa-trash").hide();
+         if(groups == 2){
+            $("#monkeybutton").hide();
+            $("#inputState").val("2 Groups (less than 20 students)");
+         }else{
+            $("#monkeybutton").show();
+            $("#inputState").val("3 Groups (more than 20 students)");
+         }
+         
+    } else {
+        // Sorry! No Web Storage support..
+        console.log("Connection to local storage is not available...");
+        tigercount = 0;
+        hippocount = 0;
+        monkeycount = 0;
+        groups = 3;
+        username = "";
+        classname = "";
+        listofbehaviors = "";
+        listofbehaviorsdisplay = "";
+    }
+}
+
+function updatelocalstorage(){
+    if (typeof(Storage) !== "undefined") {
+        // Code for localStorage/sessionStorage.
+        console.log("Connection to local storage established...");
+        localStorage.tigercount = tigercount;
+        localStorage.hippocount = hippocount;
+        localStorage.monkeycount = monkeycount;
+        localStorage.groups = groups;
+        localStorage.UTTECusername = username;
+        localStorage.classname = classname;
+        localStorage.listofbehaviors = listofbehaviors;
+        localStorage.listofbehaviorsdisplay = listofbehaviorsdisplay;
+        //$("#listbehaviorsdisplay").html(JSON.parse(listofbehaviors));
+        //document.getElementById("listbehaviordisplay").getElementsByClassName("fa-trash")[0].style.display = "none";
+         //$("#listbehaviordisplay .fa-trash").hide();
+    } else {
+        // Sorry! No Web Storage support..
+        console.log("Connection to local storage is not available...")
+    }
+}
+
+function clearlocalstorage(){
+    if (typeof(Storage) !== "undefined") {
+        // Code for localStorage/sessionStorage.
+        console.log("Connection to local storage established...");
+        localStorage.removeItem("tigercount");
+        localStorage.removeItem("hippocount");
+        localStorage.removeItem("monkeycount");
+        localStorage.removeItem("groups");
+        localStorage.removeItem("UTTECusername");
+        localStorage.removeItem("classname");
+        localStorage.removeItem("listofbehaviors");
+        localStorage.removeItem("listofbehaviorsdisplay");
+        console.log("Local storage is cleared...");
+    } else {
+        // Sorry! No Web Storage support..
+        console.log("Connection to local storage is not available...")
+    }
+}
+
+function drawAxisTickColors() { 
+    if(groups == 3){
+      var data = google.visualization.arrayToDataTable([
+         ['Group', 'Count', { role: 'style' }],
+         ['Tiger', tigercount, 'red'],
+         ['Hippo', hippocount, 'blue'],
+         ['Monkey',monkeycount,'green'],  
+      ]);
+    } else{
+        var data = google.visualization.arrayToDataTable([
+         ['Group', 'Count', { role: 'style' }],
+         ['Tiger', tigercount, 'red'],
+         ['Hippo', hippocount, 'blue'],  
+      ]);
+    }
+    
+      var options = {
+         'legend':'none',
+          vAxis:{format:'0'}
+      };
+
+      var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+      chart.draw(data,options);
+}
+
+$(document).ready(function(){
+    console.log("Document load event");
+    
+    setdatafromlocalstorage();
+    
+    // Sign in username and classname - no account required
+    //$('#Modal').modal('show');
+    
+    $("#tigerbutton").click(function(){
+        tigercount++;
+        drawAxisTickColors();
+        updatelocalstorage();
+    });
+    
+    $("#hippobutton").click(function(){
+        hippocount++;
+        drawAxisTickColors();
+        updatelocalstorage();
+    });
+    
+    $("#monkeybutton").click(function(){
+        monkeycount++;
+        drawAxisTickColors();
+        updatelocalstorage();
+    });
+    
+    $("#addbehavior").click(function(){
+        console.log("click event add behavior");
+        var behavior = $("#inputBehavior").val();
+        console.log(behavior);
+        $("#listbehaviors").append("<li class='list-group-item'>"+behavior+"<i class='fas fa-trash float-right'></li>");
+        $("#listbehaviorsdisplay").append("<li class='list-group-item'>"+behavior+"</li>");
+        listofbehaviors = JSON.stringify($("#listbehaviors").html());
+        listofbehaviorsdisplay = JSON.stringify($("#listbehaviorsdisplay").html());
+        console.log("List of behavior in string format :"+listofbehaviors);
+        updatelocalstorage();
+    });
+    
+    /*$("#listbehaviors i").click(function(){
+       $(this).parent().remove(); 
+    });*/
+    
+    $(document).on('click', '#listbehaviors i', function(){
+        var index = $(this).parent().index() + 1;
+        console.log(index);
+        $("ul.listitems li:nth-child("+index+")").remove();
+        //$(this).parent().remove();
+        listofbehaviors = JSON.stringify($("#listbehaviors").html());
+         console.log("List of behavior in string format :"+listofbehaviors);
+        listofbehaviorsdisplay = JSON.stringify($("#listbehaviorsdisplay").html());
+         updatelocalstorage();
+    });
+    
+    $("#inputState").click(function(){
+        var option = $("#inputState option:selected").text(); 
+        console.log(option);
+        if(option == "2 Groups (less than 20 students)"){
+            $("#monkeybutton").hide();
+            groups = 2;
+            drawAxisTickColors();
+            updatelocalstorage();
+        }else{
+            $("#monkeybutton").show();
+            groups = 3;
+            drawAxisTickColors();
+            updatelocalstorage();
+        }
+    });
+    
+    $("#settings").click(function(){
+        $("#settingsmenu").fadeToggle();
+    })
+    
+    /*$("#user").click(function(){
+         $('#Modal').modal('show'); 
+    });*/
+    
+    /*$("#form").click(function(){
+        //window.open("evaluation.html");
+    })*/
+    
+    $("#btn_login").click(function(){
+         username = $("#input_username").val();
+         classname = $("#input_class").val();
+         //$("title").text(username);
+         //$(".navbar-brand").html("<img src='assets/logo.svg' width='75' height='75' alt=''>Behavioral Management: "+classname);
+         //listofbehaviors = JSON.stringify($("#listbehaviors").html());
+         //listofbehaviorsdisplay = JSON.stringify($("#listbehaviorsdisplay").html());
+         //console.log("List of behavior in string format :"+listofbehaviors);
+         updatelocalstorage();
+         document.getElementById("landing").classList.add("d-none");
+         document.getElementById("navbar").classList.remove("d-none");
+         document.getElementById("game").classList.remove("d-none");
+         drawAxisTickColors();
+    });
+    
+    $("#signout").click(function(){
+         username = "";
+         $("#input_username").val(username);
+         classname = "";
+         $("#input_class").val(classname);
+         //$("title").text(username);
+         //$(".navbar-brand").html("<img src='assets/logo.svg' width='75' height='75' alt=''>Behavioral Management: "+classname);
+         clearlocalstorage();
+         location.reload();
+    });
+    
+    /*$("#closebutton, .close").click(function(){
+         drawAxisTickColors(); 
+    });*/
+});
+
+
+
